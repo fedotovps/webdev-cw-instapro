9319898fe56801bc2dc0e10b3a0cc474f3a114fb
fedotovps <fedotovps@bk.ru>
2024-03-01 20:47:46 +0300

    feat: Компонент загрузки фото на форме доб. поста

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,38 +1,34 @@
 import { renderHeaderComponent } from "./header-component.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
 
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  let imageUrl = "";
+
   const render = () => {
     // TODO: Реализовать страницу добавления поста
-    //   const appHtml = `
-    //   <div class="page-container">
-    //     <div class="header-container"></div>
-    //     Cтраница добавления поста
-    //     <button class="button" id="add-button">Добавить</button>
-    //   </div>
-    // `;
 
     const appHtml = `
-  <div class="page-container">
-    <div class="header-container"></div>
-    <div class="form">
-      <h3 class="form-title">Добавить пост</h3>
-      <div class="form-inputs">
-        <div class="upload-image-container">
-          <div class="upload=image">
-            <label class="file-upload-label secondary-button">
-                <input type="file" class="file-upload-input" style="display:none">
-                Выберите фото
-            </label> 
+    <div class="page-container">
+      <div class="header-container"></div>
+      <div class="form">
+        <h3 class="form-title">Добавить пост</h3>
+        <div class="form-inputs">
+          <div class="upload-image-container">
+            <div class="upload=image">
+              <label class="file-upload-label secondary-button">
+                  <input type="file" class="file-upload-input" style="display:none">
+                  Выберите фото
+              </label> 
+            </div>
           </div>
+          <label>
+            Опишите фотографию:
+            <textarea class="input textarea" rows="4"></textarea>
+          </label>
+          <button class="button" id="add-button">Добавить</button>
         </div>
-        <label>
-          Опишите фотографию:
-          <textarea class="input textarea" rows="4"></textarea>
-        </label>
-        <button class="button" id="add-button">Добавить</button>
       </div>
     </div>
-  </div>
   `;
 
     appEl.innerHTML = appHtml;
@@ -50,4 +46,15 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   renderHeaderComponent({
     element: document.querySelector(".header-container"),
   });
+
+  const uploadImageContainer = appEl.querySelector(".upload-image-container");
+
+  if (uploadImageContainer) {
+    renderUploadImageComponent({
+      element: appEl.querySelector(".upload-image-container"),
+      onImageUrlChange(newImageUrl) {
+        imageUrl = newImageUrl;
+      },
+    });
+  }
 }
